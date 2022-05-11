38202f45d7e82de704f810d2c643736f9e2d0ec0
Werefox <jack@wrightus.com>
2022-05-11 18:24:56 -0400

    Implemented the delete route

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -21,7 +21,8 @@ breads.get('/new', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
 	if (Bread[req.params.arrayIndex]) {
 		res.render('Show', {
-			bread: Bread[req.params.arrayIndex]
+			bread: Bread[req.params.arrayIndex],
+			index: req.params.arrayIndex
 		})
 	} else {
 		res.render('404')
@@ -30,16 +31,25 @@ breads.get('/:arrayIndex', (req, res) => {
 
 // create
 breads.post('/', (req, res) => {
-	if (!req.body.image) {
+	// validate image
+	if (!req.body.image || !req.body.image.startsWith('https') || req.body.image.startsWith('http')) {
 		req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
 	}
+	// switch checkbox state from 'on' to true/false
 	if (req.body.hasGluten === 'on') {
 		req.body.hasGluten = 'true'
 	} else {
 		req.body.hasGluten = 'false'
 	}
+	// push the changes to the database and redirect to the index page
 	Bread.push(req.body)
 	res.redirect('/breads')
+})
+
+// delete
+breads.delete('/:arrayIndex', (req, res) => {
+	Bread.splice(req.params.arrayIndex, 1)
+	res.status(303).redirect('/breads')
 })
 
 module.exports = breads
