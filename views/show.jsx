7cab00dba212572118b2fa466f96c92e3116195d
Werefox <jack@wrightus.com>
2022-05-11 19:52:02 -0400

    Styled the show page to make the buttons look nice

--- views/show.jsx
+++ views/show.jsx
@@ -16,12 +16,13 @@ function Show({bread, index}) {
 			</p>
 			<img src={bread.image} alt={bread.name} />
 			<hr />
-			<a href='/breads'><button>Back</button></a>
-			
-			<a href={`/breads/${index}/edit`}><button>Edit</button></a>
-			<form action={`/breads/${index}?_method=DELETE`} method='POST'>
-				<input type='submit' value='DELETE'/>
-			</form>
+			<div className='buttons'>
+				<a href='/breads'><button>Back</button></a>
+				<a href={`/breads/${index}/edit`}><button>Edit</button></a>
+				<form action={`/breads/${index}?_method=DELETE`} method='POST'>
+					<input type='submit' value='DELETE'/>
+				</form>
+			</div>
 		</Default>
 	)
 }
