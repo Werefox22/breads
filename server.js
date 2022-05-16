8264053b1ccae9334b81fadd964c1bcbe3d436dd
Werefox <jack@wrightus.com>
2022-05-16 19:07:01 -0400

    Installed and configured mongoose

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 // config
 require('dotenv').config()
@@ -14,6 +15,11 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 
 app.use(methodOverride('_method'))
+
+// mongoose
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+	() => { console.log('Connected to mongo: ', process.env.MONGO_URI) }	
+)
 
 // routes
 app.get('/', (req, res) => {
