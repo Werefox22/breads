1c57a354cb2c76297518a787a967ab9e9b0a1d67
Werefox <jack@wrightus.com>
2022-05-21 10:04:31 -0400

    Added seed route

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -3,6 +3,17 @@ const baker = express.Router()
 const Baker = require('../models/baker.js')
 const bakerSeedData = require('../models/baker_seed.js')
 
-
+baker.get('/data/seed', (req, res) => {
+	Baker.insertMany(bakerSeedData)
+		.then(() => {
+			res.redirect('/breads')
+		})
+		.catch(err => {
+			console.log("ERROR: ", err)
+			res.render('error', {
+				content: err
+			})
+		})
+})
 
 module.exports = baker
