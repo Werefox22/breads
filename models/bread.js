65c6df5520659473de8d3595cd3110289bdcf370
Werefox <jack@wrightus.com>
2022-05-21 10:24:11 -0400

    Fixed the bread schema's baker reference to properly accept the data when creating a new bread

--- models/bread.js
+++ models/bread.js
@@ -13,14 +13,13 @@ const breadSchema = new Schema({
 		default: 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80' 
 	},
 	baker: { 
-		type: {
-			type: Schema.Types.ObjectId,
-			ref: 'Baker'
-		}
+		type: Schema.Types.ObjectID,
+		ref: 'Baker'
 	}
 })
 
 breadSchema.methods.getBakedBy = function() {
+	console.log(this.baker)
 	return `${this.name} was baked with love by ${this.baker}`
 }
 
